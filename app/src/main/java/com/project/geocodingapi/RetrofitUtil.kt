9b531403f018267aaa5f4a5e8e41e74d9d11d0b5
k <free.rjs102@gmail.com>
2022-06-23 03:26:44 +0900

    commit

--- app/src/main/java/com/project/geocodingapi/RetrofitUtil.kt
+++ app/src/main/java/com/project/geocodingapi/RetrofitUtil.kt
@@ -7,14 +7,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitUtil {
-
     val mapApiService: MapApiService by lazy {
         getRetrofit().create(MapApiService::class.java)
     }
 
     // TMAP API 서버에 요청을 보내고 받아서 json
     private fun getRetrofit(): Retrofit {
-
         return Retrofit.Builder()
             .baseUrl(Url.TMAP_URL)
             .addConverterFactory(GsonConverterFactory.create())
